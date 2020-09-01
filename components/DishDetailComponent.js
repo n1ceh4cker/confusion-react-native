9ddf51398b256b2bf55443573f4c2151448ce1fb
Nice Akhtar <niceakhtar21@gmail.com>
2020-09-01 11:14:36 +0530

    Icons & Buttons

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
-import { Card } from 'react-native-elements'
+import { Card, Icon } from 'react-native-elements'
 import { DISHES } from '../shared/dishes'
+import { COMMENTS } from '../shared/comments'
+import { FlatList, ScrollView } from 'react-native-gesture-handler'
 
-function RenderDish({ dish }) {
+function RenderComments({comments }){
+    const renderCommentItem = ({ item, index }) => {
+        return(
+            <View key={index} style={{margin: 10}}>
+                <Text style={{fontSize: 14}}>{item.comment}</Text>
+                <Text style={{fontSize: 12}}>{item.rating} Star</Text>
+                <Text style={{fontSize: 12}}>{'-- ' + item.author + ', ' + item.date}</Text>
+            </View>
+        )
+    }
+    return(
+        <Card title='Comments'>
+            <FlatList
+                data={comments}
+                renderItem={renderCommentItem}
+                keyExtractor={item => item.id.toString()}
+            />
+        </Card>
+    )
+}
+
+function RenderDish({ dish, favourite , onPress}) {
     return dish!=null? 
         (
             <Card 
                 featuredTitle={dish.name}
                 image={require('./images/uthappizza.png')}
-                >
-                    <Text style={{ padding: 10 }}>{dish.description}</Text>
+            >
+                <Text style={{ padding: 10 }}>{dish.description}</Text>
+                <Icon
+                    raised
+                    reverse
+                    name={favourite ? 'heart':'heart-o'}
+                    type='font-awesome'
+                    color='#f50'
+                    onPress={() => favourite ? console.log('already favorite') : onPress()}
+                />
             </Card>
         ):
         (<View></View>)
@@ -18,15 +49,28 @@ function RenderDish({ dish }) {
 }
 class DishDetail extends Component {
     state = {
-        dishes: DISHES
+        dishes: DISHES,
+        comments: COMMENTS,
+        favourites: []
     }
     static navigationOptions = {
         title: 'Dish Details'
     }
+    markFavourite = (dishId) => {
+        this.setState({
+            favourites : [...this.state.favourites, dishId]
+        })
+    }
     render(){
         const dishId = this.props.navigation.getParam('dishId', '')
         return (
-            <RenderDish dish={this.state.dishes[+dishId]} />
+            <ScrollView>
+                <RenderDish dish={this.state.dishes[+dishId]} 
+                    favourite={this.state.favourites.some(e => e === dishId)}
+                    onPress={() => this.markFavourite(dishId)}
+                />
+                <RenderComments comments={this.state.comments.filter(comment => comment.id===dishId)} />
+            </ScrollView>
         )
     }
 }
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -6,27 +6,41 @@ import About from './AboutComponent'
 import Contact from './ContactComponent'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
-import { createDrawerNavigator } from 'react-navigation-drawer'
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer'
+import { StyleSheet, SafeAreaView, View, Image, Text } from 'react-native'
+import { Icon } from 'react-native-elements'
+import { ScrollView } from 'react-native-gesture-handler'
 
 const HomeNavigator = createStackNavigator(
     {
         Home: { screen: Home },
     },
     {
-    defaultNavigationOptions: {
+    defaultNavigationOptions: ({ navigation }) => ({
         headerStyle: {
             backgroundColor: '#512da8'
         },
         headerTintColor: '#fff',
         headerTitleStyle: {
             color: '#fff'
-        }
-    }
+        },
+        headerLeft: <Icon name='menu' size={24}
+                    color='white'
+                    onPress={() => navigation.toggleDrawer()}
+                    />
+    })
 })
 
 const MenuNavigator = createStackNavigator(
     {
-        Menu: { screen: Menu },
+        Menu: { screen: Menu ,
+            navigationOptions: ({ navigation }) => ({
+                headerLeft: <Icon name='menu' size={24}
+                    color='white'
+                    onPress={() => navigation.toggleDrawer()}
+                    />
+            })
+        },
         DishDetail: { screen: DishDetail }
     },
     {
@@ -45,62 +59,118 @@ const AboutNavigator = createStackNavigator(
         About: { screen: About },
     },
     {
-    defaultNavigationOptions: {
+    defaultNavigationOptions: ({ navigation }) => ({
         headerStyle: {
             backgroundColor: '#512da8'
         },
         headerTintColor: '#fff',
         headerTitleStyle: {
             color: '#fff'
-        }
-    }
+        },
+        headerLeft: <Icon name='menu' size={24}
+                    color='white'
+                    onPress={() => navigation.toggleDrawer()}
+                    />
+    })
 })
 const ContactNavigator = createStackNavigator(
     {
         Contact: { screen: Contact },
     },
     {
-    defaultNavigationOptions: {
+    defaultNavigationOptions: ({ navigation }) => ({
         headerStyle: {
             backgroundColor: '#512da8'
         },
         headerTintColor: '#fff',
         headerTitleStyle: {
             color: '#fff'
-        }
-    }
+        },
+        headerLeft: <Icon name='menu' size={24}
+                    color='white'
+                    onPress={() => navigation.toggleDrawer()}
+                    />
+    })
 })
+const CustomDrawerContent = (props) => (
+    <ScrollView>
+        <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never'}}>
+            <View style={styles.drawerHeader}>
+                <View style={{flex: 1}}>
+                    <Image source={require('./images/logo.png')} style={styles.drawerImage} />
+                </View>
+                <View style={{flex: 2}}>
+                    <Text style={styles.drawerHeaderText}>Ristorante Con Fusion</Text>
+                </View>
+            </View>
+            <DrawerItems {...props} />
+        </SafeAreaView>
+    </ScrollView>
+) 
 const MainNavigator = createAppContainer(createDrawerNavigator({
     Home: {
         screen: HomeNavigator,
         navigationOptions: {
             title: 'Home',
-            drawerLabel: 'Home'
+            drawerLabel: 'Home',
+            drawerIcon: ({ tintColor }) => (
+                <Icon 
+                    name='home'
+                    type='font-awesome'
+                    size={24}
+                    color={tintColor}
+                />
+            )
         }
     },
     About: {
         screen: AboutNavigator,
         navigationOptions: {
             title: 'About',
-            drawerLabel: 'About Us'
+            drawerLabel: 'About Us',
+            drawerIcon: ({ tintColor }) => (
+                <Icon 
+                    name='info-circle'
+                    type='font-awesome'
+                    size={24}
+                    color={tintColor}
+                />
+            )
         }
     },
     Menu: {
         screen: MenuNavigator,
         navigationOptions: {
             title: 'Menu',
-            drawerLabel: 'Menu'
+            drawerLabel: 'Menu',
+            drawerIcon: ({ tintColor }) => (
+                <Icon 
+                    name='list'
+                    type='font-awesome'
+                    size={24}
+                    color={tintColor}
+                />
+            )
         }
     },
     Contact: {
         screen: ContactNavigator,
         navigationOptions: {
             title: 'Contact',
-            drawerLabel: 'Contact Us'
+            drawerLabel: 'Contact Us',
+            drawerIcon: ({ tintColor }) => (
+                <Icon 
+                    name='address-card'
+                    type='font-awesome'
+                    size={22}
+                    color={tintColor}
+                />
+            )
         }
     }},
     {
-        drawerBackgroundColor: '#d1c4e9'
+        drawerBackgroundColor: '#d1c4e9',
+        contentComponent: CustomDrawerContent
     }
 ))
 
@@ -111,3 +181,27 @@ export default class Main extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    drawerHeader: {
+        backgroundColor: '#512da8',
+        height: 140,
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1,
+        flexDirection: "row"
+    },
+    drawerHeaderText: {
+        color: 'white',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    drawerImage: {
+        margin: 10,
+        width: 80,
+        height: 60
+    }
+})
